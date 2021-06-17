5646b6f84cfad7b892086db297413b4da62db6a7
George_Chakama <gtchakama@gmail.com>
2021-06-17 21:19:30 +0200

    Scroll animation

--- website/src/script/main.js
+++ website/src/script/main.js
@@ -100,3 +100,23 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(
+  `.home__data, .home__img,
+            .about__data, .about__img,
+            .services__content, .menu__content,
+            .app__data, .app__img,
+            .contact__data, .contact__button,
+            .footer__content`,
+  {
+    interval: 200,
+  }
+);
