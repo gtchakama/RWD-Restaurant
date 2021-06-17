d6e76a9ebcb6408ff8b36536bdc5f9b67a8b8889
George_Chakama <gtchakama@gmail.com>
2021-06-17 21:10:08 +0200

    Responsive - MENU MOBILE

--- website/src/script/main.js
+++ website/src/script/main.js
@@ -12,3 +12,15 @@ const showMenu = (toggleId, navId) =>{
     }
 }
 showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
